9b31ea53731a1e9cc388a0d613b5afa05f5f5296
lxh21k <lxh1030402003@gmail.com>
2020-11-12 14:19:20 +0800

    2020_11_12_1

--- String/longestCommonPrefix.cpp
+++ String/longestCommonPrefix.cpp
@@ -24,3 +24,53 @@ class Solution{
             return str1.substr(0, index);
         }
 };
+
+class Solution1{
+    public:
+        string longestCommonPrefix1(vector<string>& strs) {
+            if(!strs.size()) return "";
+            int length = strs[0].size();
+            int count = strs.size();
+            for(int i = 0; i<length; ++i){
+                char c = strs[0][i];
+                for(int j = 1; j<count; ++j){
+                    if(i == strs[j].size() || strs[j][i] != c) {
+                        return strs[0].substr(0, i);
+                    }
+                }
+            }
+            return strs[0];
+        }
+};
+
+class Solution2 {
+    public: 
+        string longestCommonPrefix2(vector<string>& strs) {
+            if(!strs.size()) return "";
+            int minLength = min_element(strs.begin(), strs.end(), [](const string& s, const string& t) {
+                    return s.size() < t.size();
+                    })->size();
+            int low = 0, high = minLength;
+            while(low < high) {
+                int mid = (high - low + 1)/2 + low;
+                if(isCommonPrefix(strs, mid)) {
+                    low = mid;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+            return strs[0].substr(0, low);
+}
+bool isCommonPrefix(const vector<string>& strs, int length) {
+    string str0 = strs[0].substr(0, length);
+    int count = strs.size();
+    for (int i = 1; i < count; ++i) {
+        string str = strs[i];
+        for (int j = 0; j < length; ++j) {
+            if (str0[j] != str[j]) return false;
+        }
+    }
+    return true;
+}
+};
