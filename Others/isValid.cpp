5e438faf7cf756c855063b994aee3b2a5e46b8d9
Lxh21k <lxh1030402003@gmail.com>
2020-12-24 10:04:15 +0800

    20.isValid1

--- Others/isValid.cpp
+++ Others/isValid.cpp
@@ -30,3 +30,18 @@ public:
 		return stk.empty();
 	}
 };
+
+class Solution1{
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char ch: s) {
+            if (ch == '[') stk.push(']');
+            else if (ch == '(') stk.push(')');
+            else if (ch == '{') stk.push('}');
+            else if (stk.empty() || ch != stk.top()) return false;
+            else stk.pop();
+        }
+        return stk.empty();
+    }
+};
