2943367c28464f60a2e5ff3588ae52c7004c9311
Lxh21k <lxh1030402003@gmail.com>
2021-01-14 22:26:08 +0800

    1018.prefixDivBy5

---
+++ Math/prefixesDivBy5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+        vector<bool> list;
+        int prefix = 0;
+        int length = A.size();
+        for (int i = 0; i < length; ++i) {
+            prefix = ((prefix << 1) + A[i]) % 5;
+            list.emplace_back(prefix == 0);
+        }
+        return list;
+    }
+};
