9a2f343df97149246691373ebc5704e887724892
Lxh21k <lxh1030402003@gmail.com>
2021-03-02 19:14:54 +0800

    304. NumMartix

---
+++ Array/NumMatrix.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+
+    }
+};
